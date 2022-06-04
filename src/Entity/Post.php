c6684ee9fafdb1581bb6c11477e3c4d4587bf41a
Mamy Christian <swiitjaden@gmail.com>
2022-06-04 18:03:36 +0300

    Add category entity + Add many to many relation between Post & Category

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Entity\Behavior\TimestampableTrait;
 use App\Repository\PostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
@@ -25,6 +27,14 @@ class Post
 
     #[ORM\Column(type: 'text')]
     private $content;
+
+    #[ORM\ManyToMany(targetEntity: Category::class, inversedBy: 'posts')]
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -63,6 +73,30 @@ class Post
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        $this->categories->removeElement($category);
 
         return $this;
     }
